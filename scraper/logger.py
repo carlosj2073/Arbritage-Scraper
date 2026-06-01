1c212d383c8ff53d5a8aa57ba035d5b652b137ca
carlosj2073 <carlosjaramillo2073@gmail.com>
2026-06-01 17:27:30 -0400

    add logger module

---
+++ scraper/logger.py
@@ -0,0 +1,25 @@
+import logging
+import os
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+   
+    # create logs directory if it doesn't exist
+    os.makedirs('logs', exist_ok=True)
+    
+    # create file handler
+    filehandler = logging.FileHandler(f"logs/{name}.log")
+    filehandler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    filehandler.setFormatter(formatter)
+    
+    if not logger.hasHandlers():
+        logger.addHandler(filehandler)
+
+
+    return logger 
+    
